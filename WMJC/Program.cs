4df7317dd5d08ba280a939cbfb4c9adbd834d3c3
Cauã <caualeu2016@gmail.com>
2024-12-30 19:40:49 -0300

    Adicionar configuração do banco de dados e tratamento de erros na inicialização

--- WMJC/Program.cs
+++ WMJC/Program.cs
@@ -1,9 +1,32 @@
+using WMJC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Config SQL 
+builder.Services.AddDbContext<WMJCDbContext>(options =>
+    options.UseSqlServer((builder.Configuration.GetConnectionString("DefaultConnection"))));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<WMJCDbContext>();
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Erro ao criar o banco de dados.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
