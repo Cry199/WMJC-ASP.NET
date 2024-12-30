fb612bc07fc4ff014619a72793f4a670e7cc7508
Cauã <caualeu2016@gmail.com>
2024-12-30 19:42:08 -0300

    Adicionar modelo GameCommentUserProfileModel com propriedades e construtores

---
+++ WMJC/Models/Game/CommentsGame/GameCommentUserProfileModel.cs
@@ -0,0 +1,22 @@
+﻿using WMJC.Models.User.Profile;
+
+namespace WMJC.Models.Game.CommentsGame
+{
+    public class GameCommentUserProfileModel
+    {
+        public GameCommentsModel Comment { get; set; }
+        public ProfileModel UserProfile { get; set; }
+
+
+        public GameCommentUserProfileModel(GameCommentsModel comment, ProfileModel userProfile)
+        {
+            Comment = comment;
+            UserProfile = userProfile;
+        }
+
+        public override string ToString()
+        {
+            return $"GameCommentUserProfileModel {{ Comment={Comment}, UserProfile={UserProfile} }}";
+        }
+    }
+}
